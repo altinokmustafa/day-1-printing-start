a9369cff780191d23d9a5b913d7c864ea4b5f73f
Mustafa Altinok <64875678+altinokmustafa@users.noreply.github.com>
2022-10-10 06:10:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+print("Hello World!")
+print("Hello World!\nHello world\nHello world!")
+
+#\n 
+#String Concatenation
+#print("Hello" +"World")
+
+print("Hello"+"Angela")
+
+print("Hello\nAngela")
+
+#space usage
+
+print("Hello" + " Angela")
+
+print("Hello " + "Angela")
+
+print("Hello" +" "+"Angela")
+
+#debugging exercise
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#The Python Input Function
+#input() function
+#input("A prompt for the user")
+
+#input("What is your name?")
+
+
+#This code prints the number of characters in a user's name.
+#print( len( input("What is your name? ") ) )
+
+
+
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
+
+#print("Hello " + input("what is your name?"))
+
+#print( len( input("What is your name?") ) ) 
+#Angela
+#6
+
+#
+
+
+
+
+
+#name = input("What is your name?")
+#length = len(name)
+#print(length)
+
+# 🚨 Don't change the code below 👇
+#a = input("a: ")
+#b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+#c = a
+#a = b
+#b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+#print("a: " + a)
+#print("b: " + b)
+
+
+#variable
+#james=6544554554
+
+#15. Variable Naming
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+#print("Hello " + input("What is your name?"))
+
